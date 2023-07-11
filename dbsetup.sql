2bf6b366da87ce5aef3876d68c02757bb8efb438
WittHub <rasmus.wittrock@gmail.com>
2023-07-11 17:26:26 +0200

    db stuff

--- dbsetup.sql
+++ dbsetup.sql
@@ -5,10 +5,11 @@ CREATE TABLE IF NOT EXISTS `accounts` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
+  `admin` bool NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
-INSERT INTO `accounts` (`id`, `username`, `password`, `email`) VALUES (1, 'test', 'test', 'test@test.com');
+INSERT INTO `accounts` (`id`, `username`, `password`, `admin`) VALUES (1, 'test', 'test', true);
 
 ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
 
@@ -18,3 +19,5 @@ CREATE TABLE IF NOT EXISTS `tasks` (
     `correct_answer` varchar(255) NOT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+SELECT * FROM accounts
